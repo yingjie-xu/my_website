3a4b25ed42117b895b43c7373805e78aa6bd6d4d
yingjiex <xuyingjie619@gmail.com>
2019-12-21 03:02:41 -0500

    change the title

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import Header from './Components/Header'
 
 class App extends Component {
+  // componentDidMount() {
+  //   document.title = "Yingjie Xu";
+  // }
+
   render() {
     return (
       <div>
